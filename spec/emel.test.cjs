4e06fdb1f58d3d5ef8859409214cdd8875d44210
Tony Brix <tony@brix.ninja>
2026-07-27 00:13:15 -0600

    chore: remove jest (#872)

--- spec/emel.test.cjs
+++ spec/emel.test.cjs
@@ -1,10 +1,12 @@
+const { describe, test } = require("node:test");
+const assert = require("node:assert/strict");
 const emel = require("../dist/emel.cjs");
 
 describe("emel", () => {
 	test("should work without errors", () => {
 		const el = emel("div*2");
-		expect(el.childNodes[0].nodeType).toBe(Node.ELEMENT_NODE);
-		expect(el.childNodes).toHaveLength(2);
+		assert.equal(el.childNodes[0].nodeType, Node.ELEMENT_NODE);
+		assert.equal(el.childNodes.length, 2);
 	});
 });
 
@@ -12,7 +14,7 @@ describe("Emel", () => {
 	test("should work without errors", () => {
 		const e = new emel();
 		const el = e.emel("div*2");
-		expect(el.childNodes[0].nodeType).toBe(Node.ELEMENT_NODE);
-		expect(el.childNodes).toHaveLength(2);
+		assert.equal(el.childNodes[0].nodeType, Node.ELEMENT_NODE);
+		assert.equal(el.childNodes.length, 2);
 	});
 });
--- spec/emel.umd.test.js
+++ spec/emel.umd.test.js
@@ -1,10 +1,13 @@
+import { describe, test } from "node:test";
+import assert from "node:assert/strict";
+
 import("../dist/emel.umd.js");
 
 describe("emel", () => {
 	test("should work without errors", () => {
 		const el = global.emel("div*2");
-		expect(el.childNodes[0].nodeType).toBe(Node.ELEMENT_NODE);
-		expect(el.childNodes).toHaveLength(2);
+		assert.equal(el.childNodes[0].nodeType, Node.ELEMENT_NODE);
+		assert.equal(el.childNodes.length, 2);
 	});
 });
 
@@ -12,7 +15,7 @@ describe("Emel", () => {
 	test("should work without errors", () => {
 		const e = new global.emel();
 		const el = e.emel("div*2");
-		expect(el.childNodes[0].nodeType).toBe(Node.ELEMENT_NODE);
-		expect(el.childNodes).toHaveLength(2);
+		assert.equal(el.childNodes[0].nodeType, Node.ELEMENT_NODE);
+		assert.equal(el.childNodes.length, 2);
 	});
 });
